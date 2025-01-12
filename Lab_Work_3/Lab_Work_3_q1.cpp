9957507dc151c3e855ac5d20b980f3085516f8b9
mmehmetgokce <gokcemehmet744@gmail.com>
2025-01-13 01:18:42 +0300

    lab work 3 added

---
+++ Lab_Work_3/Lab_Work_3_q1.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        arr[i] = i + 1;
+    }
+
+    cout << "Size of vector is: " << arr.size() << endl;
+    cout << "Vector elements: ";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    vector<int> originalArr = arr;
+
+    arr.insert(arr.begin(), 0);
+    cout << "Size of updated vector is: " << arr.size() << endl;
+    cout << "Updated vector elements: ";
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Updated vector in reverse order: ";
+    for (auto it = arr.rbegin(); it != arr.rend(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    arr.erase(arr.begin());
+
+    cout << "Updated vector in reverse order after removing the first element: ";
+    for (auto it = arr.rbegin(); it != arr.rend(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ Lab_Work_3/Lab_Work_3_q2.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void displayVector(const vector<int>& vec);
+void bubbleSort(vector<int>& vec);
+void insertionSort(vector<int>& vec);
+
+int main() {
+    int size = 5;
+    vector<int> vec;
+
+    for (int i = 1; i <= size; ++i) {
+        vec.push_back(i);
+    }
+
+    cout << "Size of vector is: " << vec.size() << endl;
+    cout << "Vector elements: ";
+    displayVector(vec);
+
+    int value1 = 0;
+    cout << "\nValue to be added: " << value1 << endl;
+    vec.insert(vec.begin(), value1);
+    cout << "Added to begin of the array" << endl;
+    displayVector(vec);
+
+    int value2 = 6;
+    cout << "\nValue to be added: " << value2 << endl;
+    vec.push_back(value2);
+    cout << "Added to end of the array" << endl;
+    displayVector(vec);
+
+    vector<int> bubbleSortedVec = vec;
+    bubbleSort(bubbleSortedVec);
+    cout << "\nBubble Sorted Element List\n";
+    displayVector(bubbleSortedVec);
+
+    vector<int> insertionSortedVec = vec;
+    insertionSort(insertionSortedVec);
+    cout << "\nInsertion Sorted Element List\n";
+    displayVector(insertionSortedVec);
+
+    return 0;
+}
+
+void displayVector(const vector<int>& vec) {
+    for (int val : vec) {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(vector<int>& vec) {
+    int n = vec.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (vec[j] > vec[j + 1]) {
+                swap(vec[j], vec[j + 1]);
+            }
+        }
+    }
+}
+
+void insertionSort(vector<int>& vec) {
+    int n = vec.size();
+    for (int i = 1; i < n; i++) {
+        int key = vec[i];
+        int j = i - 1;
+
+        while (j >= 0 && vec[j] < key) {
+            vec[j + 1] = vec[j];
+            j = j - 1;
+        }
+        vec[j + 1] = key;
+    }
+}
